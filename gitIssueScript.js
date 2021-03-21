adc846edab83472304f4320984afe8ddb44de106
mushthaq33 <mushthaq33@gmail.com>
2021-03-21 12:03:20 +0530

    Update gitIssueScript.js

--- gitIssueScript.js
+++ gitIssueScript.js
@@ -92,7 +92,7 @@ function callGitTotalGet(url) {
 
 function postWebhook() {
     //var webhookUrl = 'https://chat.googleapis.com/v1/spaces/AAAAdxwFenw/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=oCHPaF32lRjxpMmTRVnogbVCC6Y1YAi6E6MAg7Zm0Dw%3D';
-    var webhookUrl = 'https://chat.googleapis.com/v1/spaces/AAAAzr6_sh0/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=PYeo5TegXEbf9DwnvcIFywATRrCq0iwlxJ3Y3rkEu38%3D?threadKey=c2FtZV90aHJlYWQ=';
+    var webhookUrl = 'https://chat.googleapis.com/v1/spaces/AAAAzr6_sh0/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=PYeo5TegXEbf9DwnvcIFywATRrCq0iwlxJ3Y3rkEu38%3D?threadKey=c2FtZV90aHJlYWQ';
     var res = request('POST', webhookUrl, {
         json: {text: textMsg},
     });
