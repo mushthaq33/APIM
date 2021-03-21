dcf330b04f845793dd97638e3dbf059d5cdc311b
mushthaq33 <mushthaq33@gmail.com>
2021-03-21 12:26:41 +0530

    Update gitIssueScript.js

--- gitIssueScript.js
+++ gitIssueScript.js
@@ -96,6 +96,6 @@ function postWebhook() {
     var res = request('POST', webhookUrl, {
         json: {text: textMsg},
     });
-    var responseMsg = JSON.parse(res.getBody('utf8'));
+    var responseMsg = res.getBody('utf8');
     console.log("=======Response Log======== " + responseMsg);
 }
