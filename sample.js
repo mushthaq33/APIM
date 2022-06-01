743601a9afce693ad66826bd79e5939a012d0575
mushthaq33 <mushthaq33@gmail.com>
2022-06-01 19:02:06 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -6,33 +6,9 @@ try {
   const payload = github.context.payload
   const actionTrigerred = payload.action
   const issueUrl = payload.issue.html_url
-  const labelsJsonArr = payload.issue.labels
   const labelName = payload.label.name
   
-  /*if (labelsJsonArr.length > 0) {
-    latestLabel = labelsJsonArr[labelsJsonArr.length - 1].name
-  }*/
-  
   console.log(`The latest label added: ${labelName}`);
-  const arrPay = JSON.stringify(labelsJsonArr, undefined, 2)
-  console.log(`The latest label added: ${arrPay}`);
-  /*if (actionTrigerred == "opened") {
-    if (latestLabel == "Priority/Critical(P1)") {
-      console.log(`A priority level critical (P1) issue has been opened. Please attend immediately!!!\n URL: ${issueUrl}`);
-    } else if (latestLabel == "Priority/High(P2)")  {
-      console.log(`A priority level high (P2) issue has been opened. Please attend!!!\n URL: ${issueUrl}`);
-    } else if (latestLabel == "Priority/Medium(P3)") {
-      console.log(`A priority level medium (P3) issue has been opened. Please attend!!!\n URL: ${issueUrl}`);
-    }
-  } else if (actionTrigerred == "labeled") {
-    if (latestLabel == "Priority/Critical(P1)") {
-      console.log(`An issue has been labeled as priority level critical (P1). Please attend immediately!!!\n URL: ${issueUrl}`);
-    } else if (latestLabel == "Priority/High(P2)")  {
-      console.log(`An issue has been labeled as priority level hight (P2). Please attend!!!\n URL: ${issueUrl}`);
-    } else if (latestLabel == "Priority/Medium(P3)") {
-      console.log(`An issue has been labeled as priority level medium (P3). Please attend!!!\n URL: ${issueUrl}`);
-    }
-  }*/
   
   if (labelName == "Priority/Critical(P1)") {
     console.log(`An issue has been labeled as priority level critical (P1). Please attend immediately!!!\n URL: ${issueUrl}`);
