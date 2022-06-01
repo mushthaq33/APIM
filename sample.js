f58eb14cc7820676892f720c12c9675497ec9de9
mushthaq33 <mushthaq33@gmail.com>
2022-06-01 18:18:10 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -3,7 +3,10 @@ const github = require('@actions/github');
 
 try {
   // Get the JSON webhook payload for the event that triggered the workflow
+  const labelsJsonArr = github.context.payload.issue.labels
+  const latestLabel = labelsJsonArr[labelsJsonArr.length - 1].name
   const payload = JSON.stringify(github.context.payload.issue.labels, undefined, 2)
+  console.log(`The latest label added: ${latestLabel}`);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
