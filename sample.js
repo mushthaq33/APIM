5b1e61e930eee6dac9d989263ae8f0f58a7d84a4
mushthaq33 <mushthaq33@gmail.com>
2022-06-01 19:42:04 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -7,16 +7,36 @@ try {
   const actionTrigerred = payload.action
   const issueUrl = payload.issue.html_url
   const labelName = payload.label.name
+  var text = ""
   
   console.log(`The latest label added: ${labelName}`);
   
   if (labelName == "Priority/Critical(P1)") {
     console.log(`An issue has been labeled as priority level critical (P1). Please attend immediately!!!\n URL: ${issueUrl}`);
+    text = "An issue has been labeled as priority level critical (P1). Please attend immediately!!!\n URL: ${issueUrl}"
   } else if (labelName == "Priority/High(P2)")  {
     console.log(`An issue has been labeled as priority level hight (P2). Please attend!!!\n URL: ${issueUrl}`);
+    text = "An issue has been labeled as priority level hight (P2). Please attend!!!\n URL: ${issueUrl}"
   } else if (labelName == "Priority/Medium(P3)") {
     console.log(`An issue has been labeled as priority level medium (P3). Please attend!!!\n URL: ${issueUrl}`);
+    text = "An issue has been labeled as priority level medium (P3). Please attend!!!\n URL: ${issueUrl}"
   }
+  
+  function postWebhook(textMsg) {
+    //var webhookUrl = 'https://chat.googleapis.com/v1/spaces/AAAAdxwFenw/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=3h2Wv7IOFE97jFk4mpCMrcJnfn2Sq1jpvic9yyPzcZE%3D';
+    //var webhookUrl = 'https://chat.googleapis.com/v1/spaces/AAAA7KUbG2g/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=VSHdFhA8DfkarEjT9EfAR-nQEwgKWNcVp9AP3eSd5UU%3D';
+    var webhookUrl = 'https://chat.googleapis.com/v1/spaces/AAAAzwUSt7Q/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=cQDo2C7S5sIezfKpxGiKHBayjK9VOYj3Ae0ohyY3OxE%3D';
+    var res = request('POST', webhookUrl, {
+        json: {
+                text: textMsg, 
+              },
+    });
+    var responseMsg = res.getBody('utf8');
+    console.log("=======Response Log======== " + responseMsg);
+}
+if (text != "") {
+  postWebhook(text);
+}
   
 } catch (error) {
   core.setFailed(error.message);
